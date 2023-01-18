d980a633f88d33fbb21417bb3692785cd7499686
alba-rm <alba.r.m.722@gmail.com>
2023-01-18 10:55:11 +0100

    Salto y camara

--- Assets/Skripts/PlayerControler.cs
+++ Assets/Skripts/PlayerControler.cs
@@ -6,11 +6,23 @@ public class PlayerControler : MonoBehaviour
 {
     int playerHealth = 3;
     public float playerSpeed = 5.5f;
+    public float jumpForce = 9f;
     string texto = "Hello World";
-    bool isGrounded = false;
+    
+
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rBody;
+    private GroundSensor sensor;
+
+    float horizontal;
+    
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rBody = GetComponent<Rigidbody2D>();
+        sensor = GameObject.Find("GroundSensor").GetComponent<GroundSensor>();
+
         playerHealth = 10;
         Debug.Log(texto);
     }
@@ -18,6 +30,22 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal"),0,0) * playerSpeed * Time.deltaTime;
+        horizontal = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(horizontal, 0, 0) * playerSpeed * Time.deltaTime;
+
+        if(horizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if(horizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if(Input.GetButtonDown("Jump") && sensor.isGrounded)
+        {
+            rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
